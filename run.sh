3823fa8cf5be30bf5f209bf9c260f5ed56d0b656
satyshef <satyshef@gmail.com>
2024-01-06 00:03:52 +0100

    Added fonts folder

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 IMAGE=text2video
 NAME=t2v
-PORT=5001
+PORT=5000
 CURRENT_DIR=$(pwd)
 SOURCE_DIR=$CURRENT_DIR/source/
 OUT_DIR=$CURRENT_DIR/out/
 
-docker run --name $NAME --rm -p $PORT:5000 -v $CURRENT_DIR/app/:/app -v $SOURCE_DIR:/app/source/ -v $OUT_DIR:/app/out/ $IMAGE
+docker run -d --rm --name $NAME -p $PORT:5000 -v $CURRENT_DIR/app/:/app -v $SOURCE_DIR:/app/source/ -v $OUT_DIR:/app/out/ $IMAGE
