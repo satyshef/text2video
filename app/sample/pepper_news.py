1e8ff9fbb19e749c2cc38bec8ee9832a65ff7177
satyshef <satyshef@gmail.com>
2024-01-12 23:20:25 +0100

    Modify pepper sample

--- app/sample/pepper_news.py
+++ app/sample/pepper_news.py
@@ -7,6 +7,8 @@ def get_config():
         "audio_file": "./source/music/track_1.mp3",
         "output_dir": "./out/",
         "intro_duration": 2,
+        "promo": "Больше новостей\nв нашем Телеграм\n\nСсылка в профиле",
+        "promo_duration": 3,
         "news_duration": 7,
         "max_str_length": 25,
         "logo_text": 'перец',
@@ -70,6 +72,20 @@ def get_drawtext_introtime(start, duration, text):
     drawtext=f"fontfile={font}:text='{text}':fontsize={fontsize}:fontcolor={fontcolor}:x={pos_x}:y={pos_y}:enable='{enable}'"
     return drawtext
 
+def get_drawtext_promo(start, duration, text):
+    text = ff.prepare_text(text)
+    end = start + duration
+    fontsize = 40
+    fontcolor = 'white'
+    font = './fonts/Geist-SemiBold.otf'
+    boxcolor = 'red@0.7'
+    pos_x = '(w-text_w)/2'
+    pos_y = '(h-text_h)/2'
+    
+    enable = f"between(t,{start},{end})"
+    drawtext=f"fontfile={font}:text='{text}':fontsize={fontsize}:fontcolor={fontcolor}:box=1:boxcolor={boxcolor}:boxborderw=40:x={pos_x}:y={pos_y}:enable='{enable}'"
+    return drawtext
+
 def get_drawtext_time(start, duration, text):
     text = ff.prepare_text(text)
     end = start + duration
@@ -124,7 +140,11 @@ def run(news):
     news_time = News.get_news_time()
     file_name = News.generate_filename(news['sample'], 'mp4')
     output_file = conf['output_dir'] + file_name
-    pos = conf['intro_duration']
+    intro_duration = conf['intro_duration']
+    promo_duration = conf['promo_duration']
+    pre_duration = intro_duration + promo_duration
+    pos = pre_duration
+    news_duration = conf['news_duration']
     # создаем размытую основу из кусков видео и наложенной музыкой
     draws = []
     news_list = news['data']
@@ -136,26 +156,28 @@ def run(news):
         if text_news == "":
             continue
         # получаем элемент drawtext
-        dt_news = get_drawtext_news(pos, conf['news_duration'], text_news)
+        dt_news = get_drawtext_news(pos, news_duration, text_news)
         if dt_news == "":
             continue
         draws.append(dt_news)
-        pos += conf['news_duration']
+        pos += news_duration
 
     if len(draws) == 0:
         return '', 'Empty news list'
-    clip_duration = conf['intro_duration'] + (conf['news_duration'] * len(news_list))
+    clip_duration = pre_duration + (news_duration * len(news_list))
     create_base(conf['pieces_dir'], output_file, conf['blur_strength'], conf['audio_file'], clip_duration)
     
-    dt_intro1 = get_drawtext_intro_1(0, conf['intro_duration'], conf['intro_text_1'])
+    dt_intro1 = get_drawtext_intro_1(0, intro_duration, conf['intro_text_1'])
     draws.append(dt_intro1)
-    dt_intro2 = get_drawtext_intro_2(0, conf['intro_duration'], conf['intro_text_2'])
+    dt_intro2 = get_drawtext_intro_2(0, intro_duration, conf['intro_text_2'])
     draws.append(dt_intro2)
-    dt_introtime = get_drawtext_introtime(0, conf['intro_duration'], news_time)
+    dt_introtime = get_drawtext_introtime(0, intro_duration, news_time)
     draws.append(dt_introtime)
+    dt_promo = get_drawtext_promo(intro_duration, promo_duration, conf['promo'])
+    draws.append(dt_promo)
     dt_logo = get_drawtext_logo(0, clip_duration, conf['logo_text'])
     draws.append(dt_logo)
-    dt_date = get_drawtext_time(conf['intro_duration'], clip_duration, news_time)
+    dt_date = get_drawtext_time(pre_duration, clip_duration, news_time)
     draws.append(dt_date)
     #run(output_file, output_file, draws, clip_duration)
     #ff.remove_metadata(result_path, result_path)
